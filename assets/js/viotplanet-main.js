d508b45ea014ebbf75261dffeec159f83156d842
Shobhit Sharma <shobhit0812@gmail.com>
2021-04-06 12:54:22 +0530

    Add files via upload

---
+++ assets/js/viotplanet-main.js
@@ -0,0 +1,14 @@
+/* Copyright (c) 2020 OneYear Technologies */
+/* Version Info : 0.1.1
+   First Created: Oct 03, 2020
+   Last Modified : Jan 19, 2021
+   Author(s) : Shobhit Sharma
+
+*/
+
+var h;
+var w;
+
+h + w;
+
+document.write(h+w);
